48c92ba8982d867fcaa8d93bacfb22de7183a13c
vinicius <vini.r.faria@hotmail.com>
2025-05-26 11:11:16 -0300

    treino de variaveis primitivas em: cadastro aluno

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+    /*
+    Crie um programa que armazene e mostre as
+    informações de um aluno: nome, idade,  nota da
+    prova, se está aprovado e sua letra inicial.
+     */
+    String nomeAluno = "vinicius";
+    int idadeAluno = 22;
+    double notaDaProva = 10;
+    boolean alunoAprovado = true;
+    char letraInicialAluno = 'v';
+
+    System.out.println("=== Cadastro de Aluno ===");
+    System.out.println("Nome do aluno " + nomeAluno);
+    System.out.println("idade do aluno " + idadeAluno);
+    System.out.println("nota da prova " + notaDaProva);
+    System.out.println("aluno aprovado? " + alunoAprovado);
+    System.out.println("letra inicial do nome do aluno " + letraInicialAluno);
+    System.out.println("=========================");
+
+
+
+
+    }
+}
